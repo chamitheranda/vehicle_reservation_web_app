760d55ffce28e90dad2bc6e594b9ba047563cd53
chamith <erandachamith322@gmail.com>
2023-09-19 14:46:56 +0530

    create CustomerDetails entity

---
+++ src/main/java/com/example/vehicle_reservation_project/entity/CustomerDetails.java
@@ -0,0 +1,57 @@
+package com.example.vehicle_reservation_project.entity;
+
+import com.vladmihalcea.hibernate.type.json.JsonType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.sql.Time;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "CustomerDetails")
+@TypeDefs({
+        @TypeDef(name = "json",typeClass = JsonType.class)
+})
+public class CustomerDetails {
+    @Id
+    @Column(name = "booking_id", length = 40)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int bookingId ;
+
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+    @Column(name = "email", length = 255, nullable = false)
+    private String email;
+
+    @Type(type = "json")
+    @Column(name = "phone", columnDefinition = "json", unique = true)
+    private ArrayList contactNumber;
+
+    @Column(name = "date" , nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @Column(name = "time" , nullable = false)
+    private Time time;
+
+    @Column(name = "location" , nullable = false , length = 255)
+    private String location;
+
+    @Column(name = "vehicle_no" , nullable = false , length = 15)
+    private String vehicleNo;
+
+    @Column(name = "mileage" , nullable = false , length = 15)
+    private int mileage;
+
+    @Column(name = "message" , nullable = false , length = 255)
+    private String message;
+}
