6a6ad2a9679994de29e5a0a458764817c597e11d
chamith <erandachamith322@gmail.com>
2023-09-19 20:36:04 +0530

    implement little endpoint

--- src/main/java/com/example/vehicle_reservation_project/controller/CustomerController.java
+++ src/main/java/com/example/vehicle_reservation_project/controller/CustomerController.java
@@ -1,11 +1,19 @@
 package com.example.vehicle_reservation_project.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin()
-@RequestMapping(path = "api/v1/customer")
+@RequestMapping(path = "/api/v1/customer")
 public class CustomerController {
+
+    @GetMapping("/end")
+    public String display(){
+        String x = "hello";
+        return x ;
+    }
+
 }
