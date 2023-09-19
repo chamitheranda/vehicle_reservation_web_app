160f2c792b5ff6e4a559ecd5156f0a2d6efb7bfd
chamith <erandachamith322@gmail.com>
2023-09-19 13:22:27 +0530

    initialize the project

---
+++ src/main/java/com/example/vehicle_reservation_project/VehicleReservationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_reservation_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleReservationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleReservationProjectApplication.class, args);
+	}
+
+}
