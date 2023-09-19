e0b457d1c662cbc5d7d28a143cb0f93bf764d1a4
chamith <erandachamith322@gmail.com>
2023-09-19 20:55:22 +0530

    inject customerservice interface to controller

--- src/main/java/com/example/vehicle_reservation_project/controller/CustomerController.java
+++ src/main/java/com/example/vehicle_reservation_project/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.example.vehicle_reservation_project.controller;
 
+import com.example.vehicle_reservation_project.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin()
 @RequestMapping(path = "/api/v1/customer")
 public class CustomerController {
+
+    @Autowired
+    CustomerRepo customerRepo ;
 
     @GetMapping("/end")
     public String display(){
