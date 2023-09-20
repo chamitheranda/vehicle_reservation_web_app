4b8ef603d3f467bcd54a668c1d2f8838b03c35c3
chamith <erandachamith322@gmail.com>
2023-09-20 19:45:04 +0530

    create deleteFutureReservation() method signature

--- src/main/java/com/example/vehicle_reservation_project/service/CustomerService.java
+++ src/main/java/com/example/vehicle_reservation_project/service/CustomerService.java
@@ -1,8 +1,11 @@
 package com.example.vehicle_reservation_project.service;
 
+import com.example.vehicle_reservation_project.DTO.RequestDTO.DeleteRequestDTO;
 import com.example.vehicle_reservation_project.DTO.RequestDTO.ReservationRequestDTO;
 
 public interface CustomerService {
 
     String insertReservationData(ReservationRequestDTO reservationRequestDTO);
+
+    String deleteFutureReservation(DeleteRequestDTO deleteRequestDTO);
 }
