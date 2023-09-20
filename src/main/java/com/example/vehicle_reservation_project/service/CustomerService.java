c017ddf023d08111e908c2709970efbed4f0db13
chamith <erandachamith322@gmail.com>
2023-09-20 21:13:57 +0530

    create getAllReservationRecords method signature

--- src/main/java/com/example/vehicle_reservation_project/service/CustomerService.java
+++ src/main/java/com/example/vehicle_reservation_project/service/CustomerService.java
@@ -2,10 +2,17 @@ package com.example.vehicle_reservation_project.service;
 
 import com.example.vehicle_reservation_project.DTO.RequestDTO.DeleteRequestDTO;
 import com.example.vehicle_reservation_project.DTO.RequestDTO.ReservationRequestDTO;
+import com.example.vehicle_reservation_project.DTO.RequestDTO.ViewAllReservationDTO;
+import com.example.vehicle_reservation_project.DTO.RequestDTO.ViewReservationResponseDTO;
+import javassist.NotFoundException;
+
+import java.util.List;
 
 public interface CustomerService {
 
     String insertReservationData(ReservationRequestDTO reservationRequestDTO);
 
     String deleteFutureReservation(DeleteRequestDTO deleteRequestDTO);
+
+    List<ViewReservationResponseDTO> getAllReservationRecords(ViewAllReservationDTO viewAllReservationDTO) throws NotFoundException;
 }
