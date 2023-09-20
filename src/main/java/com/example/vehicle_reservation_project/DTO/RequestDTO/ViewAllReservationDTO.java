900c04a792cda6ae04604eb574f25b9ae7e3b886
chamith <erandachamith322@gmail.com>
2023-09-20 21:16:56 +0530

    create ViewAllReservationDTO

---
+++ src/main/java/com/example/vehicle_reservation_project/DTO/RequestDTO/ViewAllReservationDTO.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_reservation_project.DTO.RequestDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ViewAllReservationDTO {
+    private String email;
+}
