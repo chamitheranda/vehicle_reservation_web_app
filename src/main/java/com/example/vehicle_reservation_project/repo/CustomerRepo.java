8cabd9787d722ec1566d2dce8ff66c27b232d985
chamith <erandachamith322@gmail.com>
2023-09-20 18:55:46 +0530

    create query methods called 'existsByEmail' and 'getByEmail'

--- src/main/java/com/example/vehicle_reservation_project/repo/CustomerRepo.java
+++ src/main/java/com/example/vehicle_reservation_project/repo/CustomerRepo.java
@@ -2,5 +2,10 @@ package com.example.vehicle_reservation_project.repo;
 import com.example.vehicle_reservation_project.entity.CustomerDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepo extends JpaRepository<CustomerDetails,Integer> {
+    boolean existsByEmail (String email);
+
+    List<CustomerDetails> getByEmail(String email);
 }
