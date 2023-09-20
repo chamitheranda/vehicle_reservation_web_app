51ecf3c8d6c1cc4e4838546be044bfb91a7e9a71
chamith <erandachamith322@gmail.com>
2023-09-20 10:34:58 +0530

    create method signature

--- src/main/java/com/example/vehicle_reservation_project/service/CustomerService.java
+++ src/main/java/com/example/vehicle_reservation_project/service/CustomerService.java
@@ -1,5 +1,8 @@
 package com.example.vehicle_reservation_project.service;
 
+import com.example.vehicle_reservation_project.DTO.RequestDTO.ReservationRequestDTO;
+
 public interface CustomerService {
 
+    String insertReservationData(ReservationRequestDTO reservationRequestDTO);
 }
