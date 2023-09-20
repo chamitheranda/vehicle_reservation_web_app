9e588750a8fe4fe5cba9898d82183cf060ce4837
chamith <erandachamith322@gmail.com>
2023-09-20 19:41:01 +0530

    create DeleteRequestDTO to get Clientside details

---
+++ src/main/java/com/example/vehicle_reservation_project/DTO/RequestDTO/DeleteRequestDTO.java
@@ -0,0 +1,18 @@
+package com.example.vehicle_reservation_project.DTO.RequestDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class DeleteRequestDTO {
+    private String email;
+    private String vehicleNo;
+}
+
+
+
+
+
