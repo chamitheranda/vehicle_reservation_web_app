1ce35c1cd324d335be7ddfdd1454b586b1f96403
chamith <erandachamith322@gmail.com>
2023-09-20 10:28:54 +0530

    create request DTO to get data from user

---
+++ src/main/java/com/example/vehicle_reservation_project/DTO/RequestDTO/ReservationRequestDTO.java
@@ -0,0 +1,28 @@
+package com.example.vehicle_reservation_project.DTO.RequestDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ReservationRequestDTO {
+    private String name;
+    private String email;
+    private ArrayList contactNumber;
+    private Date date;
+    private LocalTime time;
+    private String location;
+    private String vehicleNo;
+    private int mileage;
+    private String message;
+}
