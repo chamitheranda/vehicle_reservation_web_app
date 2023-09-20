436bef724c9e6722f7b229f4f44aeddbfa8a1165
chamith <erandachamith322@gmail.com>
2023-09-20 18:52:48 +0530

    validate User entered data or time are sunday and times are 10 , 11 , 12

--- src/main/java/com/example/vehicle_reservation_project/controller/CustomerController.java
+++ src/main/java/com/example/vehicle_reservation_project/controller/CustomerController.java
@@ -3,7 +3,10 @@ package com.example.vehicle_reservation_project.controller;
 import com.example.vehicle_reservation_project.DTO.RequestDTO.ReservationRequestDTO;
 import com.example.vehicle_reservation_project.repo.CustomerRepo;
 import com.example.vehicle_reservation_project.service.CustomerService;
+import com.example.vehicle_reservation_project.util.CompareDate;
+import com.example.vehicle_reservation_project.util.CurrentDateTime;
 import com.example.vehicle_reservation_project.util.StandardResponse;
+import com.example.vehicle_reservation_project.util.SundayOrNot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +20,45 @@ public class CustomerController {
     @Autowired
     CustomerService customerService ;
 
+    @Autowired
+    CurrentDateTime currentDateTime;
+
+    @Autowired
+    SundayOrNot sundayOrNot ;
+
+    @Autowired
+    CompareDate compareDate ;
+
     @PostMapping("/make-reservation")
     public ResponseEntity<StandardResponse> makeReservation(@RequestBody ReservationRequestDTO reservationRequestDTO){
-        String text = customerService.insertReservationData(reservationRequestDTO);
+        String time1 = reservationRequestDTO.getTime();
+        int time = Integer.parseInt(time1);
+        if(!sundayOrNot.isSunday(reservationRequestDTO.getDate()) && compareDate.isFuture(reservationRequestDTO.getDate())){
+            if(time == 10 | time == 11 | time == 12){
+                String text = customerService.insertReservationData(reservationRequestDTO);
+                return new ResponseEntity<StandardResponse>(
+                        new StandardResponse
+                                (
+                                        201,
+                                        "reservation successfully!",
+                                        text
+                                ), HttpStatus.CREATED);
+            }
+            return new ResponseEntity<StandardResponse>(
+                    new StandardResponse
+                            (
+                                    400,
+                                    "reservation failed!",
+                                    "time is not 10,11,12"
+                            ), HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<StandardResponse>(
                 new StandardResponse
                         (
-                                201,
-                                "reservation successfully!",
-                                text
-                        ), HttpStatus.CREATED);
-    }
+                                400,
+                                "reservation failed!",
+                                "data is sunday or old date"
+                        ), HttpStatus.BAD_REQUEST);
+        }
 
 }
